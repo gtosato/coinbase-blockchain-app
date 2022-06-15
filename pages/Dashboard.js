a47418b93d28f85c4f52ae298d48c1a2eaaea13f
Greg Tosato <gregtosato@gmail.com>
2022-06-15 20:39:29 +1000

    Completed Project

---
+++ pages/Dashboard.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Sidebar from '../components/Sidebar'
+import { ethers } from 'ethers'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    process.env.NEXT_PUBLIC_METAMASK_KEY,
+    ethers.getDefaultProvider(
+      'https://rinkeby.infura.io/v3/b14e52f7a5704df5adec505e95664410'
+    )
+  )
+)
+
+const Dashboard = ({ address }) => {
+  const [sanityTokens, setSanityTokens] = useState([])
+  const [thirdWebTokens, setThirdWebTokens] = useState([])
+
+  useEffect(() => {
+    const getSanityAndThirdWebTokens = async () => {
+      const coins = await fetch(
+        "https://bcszislf.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D'coins'%5D%20%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%2C%0A%7D"
+      )
+      const sanityTokens = (await coins.json()).result
+      setSanityTokens(sanityTokens)
+
+      setThirdWebTokens(
+        sanityTokens.map((token) => sdk.getTokenModule(token.contractAddress))
+      )
+    }
+
+    getSanityAndThirdWebTokens()
+  }, [])
+
+  return (
+    <Wrapper>
+      <Sidebar />
+      <MainContainer>
+        <Header
+          walletAddress={address}
+          sanityTokens={sanityTokens}
+          thirdWebTokens={thirdWebTokens}
+        />
+        <Main
+          walletAddress={address}
+          sanityTokens={sanityTokens}
+          thirdWebTokens={thirdWebTokens}
+        />
+      </MainContainer>
+    </Wrapper>
+  )
+}
+
+export default Dashboard
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+  overflow: scroll;
+`
+
+const MainContainer = styled.div`
+  flex: 1;
+`
